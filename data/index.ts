ca3961f2602e3755c52f823601b8fce3e99178c5
hs_oh <hs_oh@korea.ac.kr>
2026-03-23 10:26:55 +0900

    test

--- data/index.ts
+++ data/index.ts
@@ -1,6 +1,3 @@
-import rawConferenceDeadlines from "./conferenceDeadlines.json";
 import { worldClocks } from "./worldClocks";
-import type { ConferenceDeadline } from "../src/types/dashboard";
 
-export const conferenceDeadlines: ConferenceDeadline[] = rawConferenceDeadlines;
 export { worldClocks };
